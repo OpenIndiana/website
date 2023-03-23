571466f46b257585a60048e86d81d87397682c20
Till Wegmueller <toasterson@gmail.com>
2023-03-23 12:05:08 +0100

    fixing website config base url
    Signed-off-by: Till Wegmueller <toasterson@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://openindiana.github.io',
-  base: '/website',
+  site: 'https://www.openindiana.org',
+  base: '',
   integrations: [mdx(), sitemap(), tailwind()]
 });
